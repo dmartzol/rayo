faeea1d6bbdf6a0ae249d19f4db6e2f6fae94408
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2021-11-29 21:08:03 -0600

    renames Ray components

--- ray.go
+++ ray.go
@@ -1,28 +1,20 @@
 package rayo
 
 type Ray struct {
-	A Vector // origin
-	B Vector // direction
+	Origin    Vector // A
+	Direction Vector // B
 }
 
 func NewRay(a, b Vector) Ray {
 	return Ray{a, b}
 }
 
-func (r Ray) Origin() Vector {
-	return r.A
-}
-
-func (r Ray) Direction() Vector {
-	return r.B
-}
-
 func (r Ray) PointAt(t float64) Vector {
-	return r.A.Add(r.B.Mul(t))
+	return r.Origin.Add(r.Direction.Mul(t))
 }
 
 func Color(ray Ray) Vector {
-	unitDirection := ray.Direction().UnitVector()
+	unitDirection := ray.Direction.UnitVector()
 	t := 0.5 * (unitDirection.Y + 1.0) // -1 < y < 1 => 0 < (y+1)*0.5 < 1.0 => 0 < t < 1
 	a := Vector{1.0, 1.0, 1.0}.Mul(1.0 - t)
 	b := Vector{.5, .7, 1.0}.Mul(t)
